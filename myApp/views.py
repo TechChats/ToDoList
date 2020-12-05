844ba01e7eea6523d879bc38c5c712e1dfeb9a3c
DonVigu <vigneshmvigi@gmail.com>
2020-12-05 23:53:31 +0530

    created to do list app |  but delete button not added

--- myApp/views.py
+++ myApp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
 from django.utils import timezone
 from myApp.models import  Todo
 
 # Create your views here.
 def home(request):
-    todo_items = Todo.objects.all().order_by('added_date' )
-    return  render(request,"myApp/index.html")
+    todo_items = Todo.objects.all().order_by('added_date')
+
+    return  render(request,"myApp/index.html", {
+        "todo_items": todo_items
+    })
 
 def add_todo(request):
     print(request.POST)
@@ -14,7 +18,8 @@ def add_todo(request):
     
     
     Created_obj = Todo.objects.create(text=content, added_date=current_data)
+    print(Created_obj.text)
+    legth_of_todos = Todo.objects.all()
+    print(legth_of_todos)
     
-    legth_of_todos = Todo.objects.all().count()
-    
-    return  render(request,"myApp/index.html")
+    return  HttpResponseRedirect("/")
